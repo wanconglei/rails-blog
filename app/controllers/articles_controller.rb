0e8963e73d202fc032a62f157f9a5e94c3fe25e2
qi y <570079893@qq.com>
2018-04-26 18:02:17 +0800

    first_commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,54 @@
+class ArticlesController < ApplicationController
+
+	before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@articles = Article.all
+	end
+
+	def show
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def edit
+	end
+
+	def create
+		@article = Article.new(article_params)
+		if @article.save
+			flash[:notice] = "event was successfully created"
+			redirect_to @article
+		else
+			render 'new'
+		end	
+	end
+
+	def update
+
+		if @article.update(article_params)
+			flash[:notice] = "event was successfully updated"
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@article.destroy
+		flash[:alert] = "event was successfully deleted"
+
+		redirect_to articles_path
+	end
+
+	private
+		def article_params
+			params.require(:article).permit(:title, :text)
+		end
+
+	def set_article
+		@article = Article.find(params[:id])
+	end
+end
